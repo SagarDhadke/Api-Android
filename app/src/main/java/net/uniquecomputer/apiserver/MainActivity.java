e0331a100b43a93040836d19bb9b2d90ace41702
Sagar Dhadke <peaceunique2003@gmail.com>
2022-07-22 20:48:25 +0530

    Git Push

---
+++ app/src/main/java/net/uniquecomputer/apiserver/MainActivity.java
@@ -0,0 +1,105 @@
+package net.uniquecomputer.apiserver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView idTxt,incomeTxt,ageTxt;
+
+    //fetch data from this url
+    String url = "https://biosagar.000webhostapp.com/";
+
+    //you send request to the server "RequestQueue" help to execute ...
+    RequestQueue myRequest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+//        button.findViewById(R.id.sub);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this,SubscriptionApi.class);
+//                startActivity(intent);
+//            }
+//        });
+
+        findViewById(R.id.sub).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SubscriptionApi.class);
+                startActivity(intent);
+            }
+        });
+
+        //initialise myRequest
+        myRequest = Volley.newRequestQueue(this);
+
+        // this are all ids like textview Button etc..
+        idTxt = findViewById(R.id.uid);
+        incomeTxt = findViewById(R.id.income);
+        ageTxt = findViewById(R.id.uage);
+
+        //This helps to click the button and get the data from the api... "gteData"
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                getData();
+            }
+        });
+
+    }
+
+    private void getData() {
+
+        // Create a object fro Json "Data is an Json From"
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,     // "JsonObjectRequest" For single object
+                new Response.Listener<JSONObject>() {                                                  // jsonRequest Null "You are creating new Request So You pass null"
+            @Override
+            public void onResponse(JSONObject response) {
+
+                try {
+                    idTxt.setText(response.get("id").toString());              //idTxt is ou fot the get "id" json data from the server...
+                    incomeTxt.setText(response.get("income").toString());
+                    ageTxt.setText(response.get("age").toString());
+
+                                //try catch meaning is app not crash
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                error.printStackTrace();              // "printStackTrace" you can see the error in the logcat
+
+            }
+        });
+
+
+        myRequest.add(request);
+
+    }
+}
---
+++ app/src/main/java/net/uniquecomputer/apiserver/SubscriptionApi.java
@@ -0,0 +1,86 @@
+package net.uniquecomputer.apiserver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DownloadManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class SubscriptionApi extends AppCompatActivity {
+
+    TextView id,uid,plan,status,payment_method,subscription_term,payment_term;
+
+    String url = "https://random-data-api.com/api/subscription/random_subscription";
+    RequestQueue myRequest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subscription_api);
+
+        myRequest = Volley.newRequestQueue(this);
+
+        id = findViewById(R.id.randomid);
+        uid = findViewById(R.id.randomuid);
+        plan = findViewById(R.id.randomplan);
+        status = findViewById(R.id.randomstatus);
+        payment_method = findViewById(R.id.randompaym);
+        subscription_term = findViewById(R.id.randomsub);
+        payment_term = findViewById(R.id.randompayte);
+
+        findViewById(R.id.fetch).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getData();
+            }
+        });
+
+    }
+
+    private void getData() {
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                try {
+
+                    id.setText(response.get("id").toString());
+                    uid.setText(response.get("uid").toString());
+                    plan.setText(response.get("plan").toString());
+                    status.setText(response.get("status").toString());
+                    payment_method.setText(response.get("payment_method").toString());
+                    subscription_term.setText(response.get("subscription_term").toString());
+                    payment_term.setText(response.get("payment_term").toString());
+
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                error.printStackTrace();
+
+            }
+        });
+
+        myRequest.add(request);
+    }
+}
